a5728e457286bb1ab09984f0dfc538ff948c4a9d
= <38575855+fishy15@users.noreply.github.com>
2025-12-23 23:16:00 -0800

    Add formatter for OCaml files

--- nvim/init.lua
+++ nvim/init.lua
@@ -169,3 +169,17 @@ vim.diagnostic.enable = true
 vim.diagnostic.config({
     virtual_text = true,
 })
+
+-- based on https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+    pattern = {"*.ml"},
+    callback = function(args)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function()
+                vim.lsp.buf.format {async = false, id = args.data.client_id }
+            end,
+        })
+    end
+})
